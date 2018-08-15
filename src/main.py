7e9edb6e49491f19ca22fbf43dbfa287e97a07b9
ziga <ziga.likar1@gmail.com>
2018-08-15 15:20:38 +0200

    Config moved to class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import os
-import json
 import numpy as np
 
 from tqdm import tqdm
 from random import shuffle
 
-config = json.loads(open('config.json', encoding='utf-8', errors='ignore').read())
+# loading config
+from services.config import Config as config
---
+++ src/services/config.py
@@ -0,0 +1,6 @@
+class Config:
+    model_name: 'self-driving-car'
+    train_dir = 'data/train/'
+    test_dir = 'data/test/'
+    img_size = 50
+    lr = 1e-3
