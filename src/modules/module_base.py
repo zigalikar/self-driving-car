394659c6978c72e35e53e7af990f912e0b6df705
ziga <ziga.likar1@gmail.com>
2018-08-21 20:59:45 +0200

    Dataset to pickle skeleton

--- src/modules/module_base.py
+++ src/modules/module_base.py
@@ -1,6 +1,7 @@
 import os.path as path
 
 import other.util as util
+import other.pickler as p
 
 # The base class for all the modules
 class ModuleBase:
@@ -23,17 +24,19 @@ class ModuleBase:
             self.dataset_train = util.load_pickle_data(pickle_train)
         else:
             self.module_log('Training pickle file does not exist - loading from dataset.')
+            self.dataset_train = p.dataset2pickle(self.path_train, pickle_train)
 
         if path.isfile(pickle_test):
             self.module_log('Loading test set from pickle file.')
             self.dataset_test = util.load_pickle_data(pickle_test)
         else:
             self.module_log('Test pickle file does not exist - loading from dataset.')
+            self.dataset_test = p.dataset2pickle(self.path_test, pickle_test)
     
     def outputModuleInit(self):
         self.module_log('\033[92mModule init successful.\033[0m')
-    #     self.module_log("Training dataset size: ", self.dataset_train['features'].shape[0])
-    #     self.module_log("Testing dataset size: ", self.dataset_test['features'].shape[0])
+        # self.module_log("Training dataset size: ", self.dataset_train['features'].shape[0])
+        # self.module_log("Testing dataset size: ", self.dataset_test['features'].shape[0])
 
     def module_log(self, msg):
         util.log('[' + self.module_name + ']: ' + msg)
---
+++ src/other/pickler.py
@@ -0,0 +1,8 @@
+import pickle
+
+import other.util as util
+
+# Saves dataset to a pickle file and returns the dataset as an object
+def dataset2pickle(data_input, pickle_output):
+    util.log('Creating a pickle file at \'' + pickle_output + '\' from dataset at \'' + data_input + '\'.')
+    pass
