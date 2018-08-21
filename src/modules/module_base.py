0c7d134e31efcd912474fbe72817ce902b537307
ziga <ziga.likar1@gmail.com>
2018-08-21 20:49:50 +0200

    Module base class changes, loading from pickle decision tree

--- src/modules/module_base.py
+++ src/modules/module_base.py
@@ -12,9 +12,7 @@ class ModuleBase:
         self.path_train, self.path_test = util.construct_dataset_paths(module_name, config)
         self.prepare_dataset()
 
-        # Accessing features and labels
-        # X_train, y_train = train['features'], train['labels']
-        # X_test, y_test = test['features'], test['labels']
+        self.outputModuleInit()
 
     def prepare_dataset(self):
         pickle_train = path.join(self.path_train, "..", self.module_name + ".train.p")
@@ -22,20 +20,20 @@ class ModuleBase:
 
         if path.isfile(pickle_train):
             self.module_log('Loading training set from pickle file.')
+            self.dataset_train = util.load_pickle_data(pickle_train)
         else:
-            self.module_log('Training pickle file does not exist - loading from images.')
+            self.module_log('Training pickle file does not exist - loading from dataset.')
 
         if path.isfile(pickle_test):
             self.module_log('Loading test set from pickle file.')
+            self.dataset_test = util.load_pickle_data(pickle_test)
         else:
-            self.module_log('Test pickle file does not exist - loading from images.')
-
-        #self.dataset_train, self.dataset_test = util.load_data(self.path_train, self.path_test)
+            self.module_log('Test pickle file does not exist - loading from dataset.')
     
-    # def outputModuleInit(self):
-    #     util.log('Module \'' + self.module_name + '\' init successful.')
-    #     util.log("Training dataset size: ", self.dataset_train['features'].shape[0])
-    #     util.log("Testing dataset size: ", self.dataset_test['features'].shape[0])
+    def outputModuleInit(self):
+        self.module_log('\033[92mModule init successful.\033[0m')
+    #     self.module_log("Training dataset size: ", self.dataset_train['features'].shape[0])
+    #     self.module_log("Testing dataset size: ", self.dataset_test['features'].shape[0])
 
     def module_log(self, msg):
         util.log('[' + self.module_name + ']: ' + msg)
